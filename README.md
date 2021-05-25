33b8b79ae89588fe9a7d2622ca217c3ffe7bc057
Günther Hutter <guenther.hutter@bytebang.at>
2021-05-25 12:25:52 +0200

    Added documentation and a way how to set the port and wwwroot for the application

--- README.md
+++ README.md
@@ -2,3 +2,34 @@
 
 # simpleserver
 A simple webserver for serving static content
+
+# usage
+The server can serve either content which is compiled into the jar file, or it can serve content which comes from an existing directory on the machine.
+
+usually the server just has to be started via commandline
+
+`java -jar simpleserver.jar`
+
+then it will serve the content by default on port 8080. If you want to change the port, then you can do this via the vm parameter _port_ like this:
+
+`java -Dport=9999 -jar simpleserver.jar`
+
+This will serve the content from within the jar file on port 9999.
+
+## serving content from within the jar
+The package `at.htlle.simpleserver.staticfiles` in the resources section of the maven project holds the files that should be served statically. Whatever is in this directory will be served by default from the server.
+
+## serving content from a directory
+If the data should not be compiled into the jar the it is possible to point the server towards a directory. This is also done via a vm parameter _wwwroot_ like this:
+
+`java -Dwwwroot=/tmp/www -jar simpleserver.jar`
+
+With the commandline from above the server will look for files within the /tmp/www directory (instead of serving content from within the jar)
+
+#compiling
+
+Compilation is done via maven:
+
+```
+mvn package
+```
--- SimpleServer/src/main/java/at/htlle/simpleserver/App.java
+++ SimpleServer/src/main/java/at/htlle/simpleserver/App.java
@@ -1,8 +1,13 @@
 package at.htlle.simpleserver;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import io.undertow.Undertow;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
+import io.undertow.server.handlers.resource.PathResourceManager;
 import io.undertow.server.handlers.resource.ResourceHandler;
+import io.undertow.server.handlers.resource.ResourceManager;
 
 /**
  * Hello world!
@@ -13,11 +18,29 @@ public class App
     public static void main( String[] args )
     {
     	
-    	ClassPathResourceManager crm = new ClassPathResourceManager(App.class.getClassLoader(), "at/htlle/simpleserver/staticfiles");
-    	
+	    // Get the port where we should serve on
+	    String port = System.getProperty("port");
+	    int portnumber = (port == null) ? 8080 : Integer.parseInt(port);
+	    
+	    ResourceManager rm = null;
+	    
+	    // Add the ability to add a environmentvariable based configuration
+	    String base = System.getProperty("wwwroot");
+
+	    
+	    if(base != null)
+	    {
+		    Path p = Paths.get(base);
+	    	rm = new PathResourceManager(p);
+	    }
+	    else
+	    {
+	    	rm = new ClassPathResourceManager(App.class.getClassLoader(), "at/htlle/simpleserver/staticfiles");
+	    }
+	    
     	Undertow server = Undertow.builder()
-                .addHttpListener(8080, "localhost")
-                .setHandler(new ResourceHandler(crm))
+                .addHttpListener(portnumber, "localhost")
+                .setHandler(new ResourceHandler(rm))
                 .build();
     	
         server.start();
